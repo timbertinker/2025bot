590a392cae8df5ffc32251fa589753a420f7a6da
timbertinker <forest310125@gmail.com>
2024-12-01 13:44:59 -0300

    feat: adding activiy

--- src/client/events/ready.ts
+++ src/client/events/ready.ts
@@ -1,13 +1,32 @@
+import { connections } from '@/models/connection.model';
 import { connect } from 'mongoose';
 import { createEvent } from 'seyfert';
+import { ActivityType, PresenceUpdateStatus } from 'seyfert/lib/types';
 
 export default createEvent({
 	data: { name: 'botReady', once: true },
 	async run(user, client) {
-		await connect(
-			'mongodb+srv://paiton:998669277@cluster0.ltucfl5.mongodb.net/test?retryWrites=true&w=majority',
+		client.logger.info(`${user.name} is ready.`);
+
+		await connect(process.env.MONGOOSE_URL as string);
+
+		client.logger.debug(
+			'Database connected successfully (mongodb+srv://*****:*****@connections.xqlmv.mongodb.net/?retryWrites=true&w=majority&appName=Connections)',
 		);
 
-		client.logger.info(`${user.name} is ready.`);
+		const connectionsCount = await connections.countDocuments();
+
+		client.gateway.setPresence({
+			activities: [
+				{
+					name: '',
+					type: ActivityType.Custom,
+					state: `${connectionsCount} amazing connections to discover 🤍`,
+				},
+			],
+			afk: false,
+			since: null,
+			status: PresenceUpdateStatus.Online,
+		});
 	},
 });
---
+++ src/utils/utility/Constants.ts
@@ -0,0 +1,3 @@
+export const Constants = {
+	InvisibleColor: '#2b2d31',
+};
